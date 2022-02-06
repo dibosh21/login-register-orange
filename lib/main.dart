a91d657828a118d4d69f9f55be5ef257dc8533ad
Dibosh <dibosh@outlook.com>
2022-02-06 23:55:28 +0600

    v.1.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:interview/MaterialApp/material_app.dart';
-import 'package:wakelock/wakelock.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Keep device awake
-  Wakelock.enable();
 
   // Omit gradiant dithering
   Paint.enableDithering = true;
